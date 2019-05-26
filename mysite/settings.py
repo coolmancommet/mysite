87c1419dcb0d1e9e1f40aab5921fadb69bb5380d
coolmancommet <50962421+coolmancommet@users.noreply.github.com>
2019-05-26 22:47:43 +0530

    'gid-and-gpass'

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='cmancommet@gmail.com'
-EMAIL_HOST_PASSWORD='commetmanc'
+EMAIL_HOST_USER=os.environ.get("GID")
+EMAIL_HOST_PASSWORD=os.environ.get("GPASS")
