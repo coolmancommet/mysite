5b92fcc09b5f130fd71f554862ca0a36d9850445
coolmancommet <50962421+coolmancommet@users.noreply.github.com>
2019-05-30 22:15:36 +0530

    deployment_changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,7 +120,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+#for heroku deployment
+django_heroku.settings(locals())
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
