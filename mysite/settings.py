094f1d801b8a02343ff0b976e8ee57a3fc112942
coolmancommet <50962421+coolmancommet@users.noreply.github.com>
2019-06-01 00:23:54 +0530

    final deployment changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='1234567890987654312341527'
+SECRET_KEY =os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'True'
+DEBUG = os.environ.get('DEBUG_VALUE')
 
 ALLOWED_HOSTS = ['192.168.29.176','localhost','vast-temple-82477.herokuapp.com']
 
