4b542ab03d57a29cd170b9233bfd0c69a335135a
coolmancommet <50962421+coolmancommet@users.noreply.github.com>
2019-05-31 22:00:35 +0530

    deployment changes

--- cu/models.py
+++ cu/models.py
@@ -9,8 +9,8 @@ class profile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
-    def save(self):
-        super().save()
+    def save(self,*args,**kawrgs):
+        super().save(*args,**kawrgs)
         img=Image.open(self.image.path)
         if img.height>300 or img.width>300:
             output_size=(300,300)
