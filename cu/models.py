68916547d9272acbdcf1b56a6cbb71d426050415
coolmancommet <50962421+coolmancommet@users.noreply.github.com>
2019-06-04 14:05:46 +0530

    s3 changes

--- cu/models.py
+++ cu/models.py
@@ -9,10 +9,10 @@ class profile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
-    def save(self,*args,**kawrgs):
-        super().save(*args,**kawrgs)
-        img=Image.open(self.image.path)
-        if img.height>300 or img.width>300:
-            output_size=(300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    # def save(self,*args,**kawrgs):
+    #     super().save(*args,**kawrgs)
+    #     img=Image.open(self.image.path)
+    #     if img.height>300 or img.width>300:
+    #         output_size=(300,300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
